64f9e02b9d56afc15d2f525236a2d1b52907e9f6
Filip Politowski <fil.politowski@gmail.com>
2024-11-03 18:01:48 +0100

    microservices architecture created

---
+++ src/main/java/pl/filip/notificationdomain/NotificationDomainApplication.java
@@ -0,0 +1,13 @@
+package pl.filip.notificationdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/filip/notificationdomain/NotificationDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filip.notificationdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
