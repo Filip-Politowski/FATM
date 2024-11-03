395d21bd8159042854f1dafeba4a54684a06e85b
Filip Politowski <fil.politowski@gmail.com>
2024-11-03 18:01:48 +0100

    microservices architecture created

---
+++ src/main/java/pl/filip/fatm/UserGroupDomainApplication.java
@@ -0,0 +1,13 @@
+package pl.filip.fatm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserGroupDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserGroupDomainApplication.class, args);
+	}
+
+}
