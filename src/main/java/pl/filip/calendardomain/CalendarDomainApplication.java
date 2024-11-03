6fbee68fad6343f7dbdb34a6d356be0a7dfcf36c
Filip Politowski <fil.politowski@gmail.com>
2024-11-03 18:01:49 +0100

    microservices architecture created

---
+++ src/main/java/pl/filip/calendardomain/CalendarDomainApplication.java
@@ -0,0 +1,13 @@
+package pl.filip.calendardomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/filip/calendardomain/CalendarDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filip.calendardomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
