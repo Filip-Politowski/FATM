c1e0231049d455b1f73dfab620594c4f5ac27020
Filip Politowski <fil.politowski@gmail.com>
2024-11-03 18:01:50 +0100

    microservices architecture created

---
+++ src/main/java/pl/filip/authdomain/AuthDomainApplication.java
@@ -0,0 +1,13 @@
+package pl.filip.authdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthDomainApplication.class, args);
+	}
+
+}
