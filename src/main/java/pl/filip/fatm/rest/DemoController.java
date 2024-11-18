ceb4880078131e435de34d7270a71693d3d71d2a
Filip Politowski <fil.politowski@gmail.com>
2024-11-19 00:19:36 +0100

    first communication via gateway

---
+++ src/main/java/pl/filip/fatm/rest/DemoController.java
@@ -0,0 +1,17 @@
+package pl.filip.fatm.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user_group")
+public class DemoController {
+
+    @GetMapping
+    public String getHelloMessageFromKeycloak() {
+        return "Hello from Keycloak!";
+    }
+
+
+}
--- src/test/java/pl/filip/fatm/UserGroupDomainApplicationTests.java
+++ src/test/java/pl/filip/fatm/UserGroupDomainApplicationTests.java
@@ -1,13 +1,13 @@
-package pl.filip.fatm;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class UserGroupDomainApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package pl.filip.fatm;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class UserGroupDomainApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
